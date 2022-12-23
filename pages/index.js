6b1968157c87e136732ae79b4481eef9222635eb
hector.munoz <hector.munoz@mxmart.mx>
2022-12-23 11:27:39 -0600

    Hola

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import { useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const getData = async () => {
+    const res = await fetch(`/api/test`);
+    const data = await res.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
       <Head>
@@ -26,7 +35,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +128,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
